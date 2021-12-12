e7c1dd80e9920f897dec4d8cfb04b27631a827ab
onlynavv <onlynavv@gmail.com>
2021-12-12 08:30:34 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,6 +3,10 @@ import { genPassword, createUser,getByUserName } from "./helper.js"
 const router = express.Router()
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
 
 router.route("/signup")
 .get((request, response)=>{
